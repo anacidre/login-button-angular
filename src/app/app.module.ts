498e1a72cb322531fc79463904b51961a95a53ea
Ana <ana@anacidre.com>
2020-04-23 17:53:38 +0200

    Define Custom Element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, Injector } from "@angular/core";
+import { createCustomElement } from "@angular/elements";
 
 import { AppComponent } from "./app.component";
 import { LoginButtonComponent } from "./login-button/login-button.component";
@@ -8,7 +9,14 @@ import { RouterModule } from "@angular/router";
 @NgModule({
   declarations: [AppComponent, LoginButtonComponent],
   imports: [BrowserModule, RouterModule.forRoot([])],
-  providers: [],
-  bootstrap: [AppComponent],
+  entryComponents: [LoginButtonComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const loginButton = createCustomElement(LoginButtonComponent, {
+      injector: this.injector,
+    }) as any;
+    customElements.define("login-button", loginButton);
+  }
+}
